18eeab57baafd69b9993216fb3bb2ef306255736
Yassine El Bouziady <elbouziady.yassine@gmail.com>
2023-05-25 18:39:24 +0100

    fix: missing env varaibles

--- e2e/App.spec.ts
+++ e2e/App.spec.ts
@@ -17,28 +17,28 @@ describe('weather app', () => {
 
     const searched = 'london'
     const expected = 'London'
-    const textField = page.getByPlaceholder('Search for a city...')
+    const textField = await page.getByPlaceholder('Search for a city...')
     await textField.fill(searched)
     await textField.press('Enter')
 
-    const locator = page.locator('h1')
+    const locator = await page.locator('h1')
     await expect(locator).toContainText(searched.at(0)?.toUpperCase() + searched.slice(1))
 
     // save location
-    const buttonSave = page.getByRole('button', { name: 'save location' })
+    const buttonSave = await page.getByRole('button', { name: 'save location' })
     await buttonSave.click()
     // go home page and check if it's added to cities list
-    const homelink = page.getByLabel('back to home page')
+    const homelink = await page.getByLabel('back to home page')
     await homelink.click()
 
-    const cityButton = page.getByRole('button', { name: expected })
+    const cityButton = await page.getByRole('button', { name: expected })
     await expect(cityButton).toBeAttached()
 
     // navigate to city page
     await page.goto(`location?q=${expected}`)
 
     // delete location
-    const buttonDelete = page.getByRole('button', { name: 'delete location' })
+    const buttonDelete = await page.getByRole('button', { name: 'delete location' })
     await buttonDelete.click()
 
     // back to home page and check if it's deleted
